c0f8160232344f50f6e82ee8378af3ef3cccc8e9
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-12-20 11:19:51 +0100

    mise à jour du css pour que la taille (height) du main s'adapte toujours au contenu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import "./components/LiveArcarde.css";
-import LiveArcade from "./components/LiveArcade";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import LiveArcade from "./components/LiveArcade";
 
 function App() {
   return (
