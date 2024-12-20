2bde87f6003a2ad75a9520d123824da88091d879
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-12-20 10:44:26 +0100

    commit supplémentaire

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
 
 function App() {
   return (
