e823373af783a7939690702eac2c679d511a29d5
Jules-wild <julesmarcilloux11@gmail.com>
2025-01-03 10:32:29 +0100

    US13 fini fetch

--- client/src/components/LiveArcade.tsx
+++ client/src/components/LiveArcade.tsx
@@ -1,55 +1,31 @@
-const ListArcade = [
-  {
-    id: 1,
-    title: "Street fighter",
-    img: "./src/assets/images/street-fighter-II.png",
-  },
-  {
-    id: 2,
-    title: "Space Invaders",
-    img: "./src/assets/images/Space_Invaders_Logo.png",
-  },
-  {
-    id: 3,
-    title: "Dragon ball fighterZ",
-    img: "./src/assets/images/Dragon_Ball_FighterZ_Logo.png",
-  },
-  {
-    id: 4,
-    title: "Pac-man",
-    img: "./src/assets/images/pac-man.gif",
-  },
-  {
-    id: 5,
-    title: "Donkey Kong",
-    img: "./src/assets/images/Donkey_Kong_Logo.png",
-  },
-];
+import { useEffect, useState } from "react";
 
-interface ListArcadeProps {
+interface Game {
   id: number;
-  title: string;
-  img: string;
+  name: string;
+  genre: string;
 }
 
 function LiveArcade() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const data = await fetch("/api/games").then((res) => res.json());
+      setGames(data);
+    };
+
+    fetchGames();
+  }, []);
+
   return (
     <div>
-      <h1 id="top">Top 5 des jeux de nos arcadeur</h1>
-      <ul id="list">
-        {ListArcade.map((game: ListArcadeProps) => (
-          <li key={game.id}>
-            <img src={game.img} alt={game.title} width={140} />
-          </li>
-        ))}
-      </ul>
-      <div id="borne">
-        <img
-          src="./src/assets/images/borne-arcade1.png"
-          alt="borne"
-          width={600}
-        />
-      </div>
+      {games.map((game) => (
+        <div key={game.id}>
+          <h3>{game.name}</h3>
+          <p>{game.genre}</p>
+        </div>
+      ))}
     </div>
   );
 }
