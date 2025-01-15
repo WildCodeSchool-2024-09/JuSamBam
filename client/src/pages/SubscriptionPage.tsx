ae7ed9f65245730dd6236b69bb0fcf6cb9017fd3
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2025-01-15 11:58:01 +0100

    création de la page d'inscription

---
+++ client/src/pages/SubscriptionPage.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import SubscriptionForm from "../components/SubscriptionForm";
+
+function SubscriptionPage() {
+  const navigate = useNavigate();
+
+  const newUser = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  return (
+    <SubscriptionForm
+      defaultValue={newUser}
+      submitted={(userData) =>
+        fetch(`${import.meta.env.VITA_API_URL}/api/users`, {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userData),
+        }).then((res) => {
+          if (res.status === 201) {
+            alert("Utilisateur crée avec succès, bienvenue parmi nous !");
+            navigate("/"); // mettre l'url de la page de login
+          }
+        })
+      }
+    >
+      Soumettre
+    </SubscriptionForm>
+  );
+}
+
+export default SubscriptionPage;
