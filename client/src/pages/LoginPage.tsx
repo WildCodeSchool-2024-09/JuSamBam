6aa4e99c0708598259cffaf9cfd0cd5f8a1be282
Nekoshinji <obito26@gmail.com>
2025-01-15 14:06:52 +0100

    US33 loginpage et composant relier

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,12 +1,44 @@
+import type React from "react";
+import LoginForm from "../components/LoginForm";
+
+type LoginDatas = {
+  username: string;
+  password: string;
+};
+
 const LoginPage: React.FC = () => {
+  const handleLoginSubmit = async (_login: LoginDatas) => {
+    // try {
+    //   const response = await fetch(`${import.meta.env.VITA_API_URL}/api/login`, {
+    //     method: "post",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(login),
+    //   });
+    //   if (response.ok) {
+    //     const data = await response.json();
+    //     console.info("Réponse API:", data);
+    //   } else {
+    //     alert("Erreur de connexion. Vérifiez vos identifiants.");
+    //   }
+    // } catch (error) {
+    //   console.error("Erreur:", error);
+    //   alert("Erreur de connexion au serveur.");
+    // }
+  };
+
+  const defaultLoginDatas: LoginDatas = {
+    username: "",
+    password: "",
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      <LoginForm defaultValue={defaultLoginDatas} submitted={handleLoginSubmit}>
+        Login
+      </LoginForm>
     </div>
   );
 };
