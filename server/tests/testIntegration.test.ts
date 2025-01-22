1b0a2dc6b71f5aa03028d5dc84c5c419e2aea52d
Nekoshinji <obito26@gmail.com>
2025-01-22 14:36:54 +0100

    Mise en place du test d'intégration

---
+++ server/tests/testIntegration.test.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+import supertest from "supertest";
+import app from "../src/app";
+import databaseClient from "../database/client";
+
+describe("Videogame API Endpoints", () => {
+  // Test GET /api/videogames : vérifier la récupération des jeux
+  test("GET /api/videogames doit retourner la liste des jeux vidéo", async () => {
+    const response = await supertest(app).get("/api/videogames");
+
+    // Vérification de la réponse
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body.length).toBeGreaterThan(0);
+    expect(response.body[0]).toHaveProperty("title");
+    expect(response.body[0]).toHaveProperty("img");
+  });
+  // Après tous les tests, fermer la connexion à la base
+  afterAll(async () => {
+    await databaseClient.end();
+  });
+});
