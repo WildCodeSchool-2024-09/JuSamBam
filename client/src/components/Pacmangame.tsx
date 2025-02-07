905f956e31e3daf83c79444268ad5982b683e169
Jules-wild <julesmarcilloux11@gmail.com>
2025-02-07 10:03:57 +0100

    Ajout de la 'r' pour reload le jeux

--- client/src/components/Pacmangame.tsx
+++ client/src/components/Pacmangame.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
 import { render } from "react-dom";
 import Pacman from "./Pacman";
 
 function PacmanGame() {
+  const [gameKey, setGameKey] = useState(0); // Clé unique pour relancer le jeu
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "r") {
+        setGameKey((prevKey) => prevKey + 1); // Change la clé pour forcer le reload avec la touche "r"
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
-    // assigniation des touches Z,Q,S,D pour ce déplacer
     <div className="pacman-game">
-      <Pacman moveUp="z" moveDown="s" moveLeft="q" moveRight="d" />
+      <Pacman
+        key={gameKey}
+        moveUp="z"
+        moveDown="s"
+        moveLeft="q"
+        moveRight="d"
+      />
     </div>
   );
 }
