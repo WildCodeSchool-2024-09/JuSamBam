0f0489d97e400da2b8488fc199d2a1d07efb81c4
Jules-wild <julesmarcilloux11@gmail.com>
2025-02-07 11:05:27 +0100

    Ajout des touches directionelle de la manette et la touche r pour relancer le jeux si on joue avec zqsd

--- client/src/components/Pacmangame.tsx
+++ client/src/components/Pacmangame.tsx
@@ -7,14 +7,54 @@ function PacmanGame() {
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "r") {
-        setGameKey((prevKey) => prevKey + 1); // Change la clé pour forcer le reload avec la touche "r"
+      if (event.code === "Enter" || event.code === "KeyR") {
+        setGameKey((prevKey) => prevKey + 1); // Change la clé pour forcer un nouveau rendu
       }
     };
 
+    let animationId: number;
+    const handleGamepadInput = () => {
+      const gamepads = navigator.getGamepads();
+      const gamepad = gamepads[0];
+
+      if (gamepad) {
+        const { buttons } = gamepad;
+
+        if (buttons[12].pressed) {
+          window.dispatchEvent(
+            new KeyboardEvent("keydown", { key: "ArrowUp" }),
+          );
+        }
+        if (buttons[13].pressed) {
+          window.dispatchEvent(
+            new KeyboardEvent("keydown", { key: "ArrowDown" }),
+          );
+        }
+        if (buttons[14].pressed) {
+          window.dispatchEvent(
+            new KeyboardEvent("keydown", { key: "ArrowLeft" }),
+          );
+        }
+        if (buttons[15].pressed) {
+          window.dispatchEvent(
+            new KeyboardEvent("keydown", { key: "ArrowRight" }),
+          );
+        }
+        if (buttons[9].pressed) {
+          // Bouton Start
+          setGameKey((prevKey) => prevKey + 1); // Change la clé pour forcer un nouveau rendu
+        }
+      }
+
+      animationId = requestAnimationFrame(handleGamepadInput);
+    };
+
     window.addEventListener("keydown", handleKeyDown);
+    animationId = requestAnimationFrame(handleGamepadInput);
+
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
+      cancelAnimationFrame(animationId);
     };
   }, []);
 
